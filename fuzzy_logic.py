ffd9e8286f5e05736403400f7909d69eda5397ff
arfan21 <arfan.jadul10@gmail.com>
2021-04-24 22:41:27 +0700

    selesai fuzzy makanan

--- fuzzy_logic.py
+++ fuzzy_logic.py
@@ -6,29 +6,29 @@ print(dataRestoran)
 
 # skala 1 - 100
 # Pelayanan Jelek (Trapesium)
-batasBawahJelekPelayanan = 20
-batasAtasJelekPelayanan = 50
+batasBawahJelekPelayanan = 20.0
+batasAtasJelekPelayanan = 50.0
 # Pelayanan Sedang (Segitiga)
-batasBawahSedangPelayanan = 40
-batasTengahSedangPelayanan = 65
-batasTengahSedangPelayanan = 65
-batasAtasSedangPelayanan = 90
+batasBawahSedangPelayanan = 40.0
+batasTengahSedangPelayanan = 65.0
+batasTengahSedangPelayanan = 65.0
+batasAtasSedangPelayanan = 90.0
 # Pelayanan Bagus (Trapesium)
-batasBawahBagusPelayanan = 80
-batasAtasBagusPelayanan = 100
+batasBawahBagusPelayanan = 80.0
+batasAtasBagusPelayanan = 100.0
 
 # skala 1 - 10
 # Makanan Tidak Enak (Trapesium)
-batasBawahTidakEnakMakanan = 3
-batasAtasTidakEnakMakanan = 5
+batasBawahTidakEnakMakanan = 3.0
+batasAtasTidakEnakMakanan = 5.0
 # Makanan Sedang (Segitiga)
-batasBawahSedangMakanan = 4
+batasBawahSedangMakanan = 4.0
 batasTengahSedangMakanan = 6.5
 batasTengahSedangMakanan = 6.5
-batasAtasSedangMakanan = 9
+batasAtasSedangMakanan = 9.0
 # Makanan Enak (Trapesium)
-batasBawahEnakMakanan = 8
-batasAtasEnakMakanan = 10
+batasBawahEnakMakanan = 8.0
+batasAtasEnakMakanan = 10.0
 
 
 def pelayanan(x):
@@ -60,4 +60,25 @@ def pelayananBagus(x):
     if (x >= batasBawahBagusPelayanan and x <= batasAtasBagusPelayanan):
         return 'besar',  ((x-batasBawahBagusPelayanan)/(batasAtasBagusPelayanan-batasBawahBagusPelayanan))
     elif (x > batasAtasBagusPelayanan):
-        return 'besar', 1
+        return 'besar', 1.0
+
+
+def makananTidakEnak(x):
+    if (x >= 0 and x <= batasBawahTidakEnakMakanan):
+        return 'kecil', 1.0
+    elif (x > batasBawahTidakEnakMakanan and x <= batasAtasTidakEnakMakanan):
+        return 'kecil', ((batasAtasTidakEnakMakanan-x)/(batasAtasTidakEnakMakanan-batasBawahTidakEnakMakanan))
+
+
+def makananSedang(x):
+    if (x >= batasBawahSedangMakanan and x <= batasTengahSedangMakanan):
+        return 'sedang',  ((x-batasBawahSedangMakanan)/(batasTengahSedangMakanan - batasBawahSedangMakanan))
+    elif (x > batasTengahSedangMakanan and x <= batasAtasSedangMakanan):
+        return 'sedang', ((batasAtasSedangMakanan-x)/(batasAtasSedangMakanan-batasTengahSedangMakanan))
+
+
+def makananEnak(x):
+    if (x >= batasBawahEnakMakanan and x <= batasAtasEnakMakanan):
+        return 'besar',  ((x-batasBawahEnakMakanan)/(batasAtasBagusPelayanan-batasBawahEnakMakanan))
+    elif (x > batasAtasEnakMakanan):
+        return 'besar', 1.0
